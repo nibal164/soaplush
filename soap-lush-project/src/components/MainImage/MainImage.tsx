9f0d579f437db7828e4847af80b092807371dfbe
Nibal <nibal358164@gmail.com>
2023-01-05 14:33:21 +0200

    message

--- soap-lush-project/src/components/MainImage/MainImage.tsx
+++ soap-lush-project/src/components/MainImage/MainImage.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./MainImage.css";
 import MainHomeImage from "../../assets/MainImageHomePage/WhatsApp Image 2022-12-22 at 11.05.45 AM.jpeg";
+import MainLogoName from "../../assets/MainLogoName/grid-01.png";
 
 function MainImage() {
   return (
     <div>
+      <section className="main-logo-image">
+        <img className="main-logo" src={MainLogoName} alt="" id="main-logo" />
+      </section>
       <section>
         <img className="main-image" src={MainHomeImage} alt="" id="logo" />
         <section className="about-us-image">
